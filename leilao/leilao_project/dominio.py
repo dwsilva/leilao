af84615e11a82027811a0d2e91971597cb3a5630
David Couto <coutodwsc@gmail.com>
2022-01-05 23:15:07 -0300

    Refatorando o código!

--- leilao/leilao_project/dominio.py
+++ leilao/leilao_project/dominio.py
@@ -1,4 +1,4 @@
-import sys
+from leilao.excecoes import LanceInvalido
 
 
 class Usuario:
@@ -15,10 +15,15 @@ class Usuario:
         return self.__carteira
 
     def propoe_lance(self, leilao, valor):
+        if not self._valor_eh_valido(valor):
+            raise LanceInvalido("Não é possível propor um lance com um valor maior do que o valor em carteira!!!")
         lance = Lance(self, valor)
         leilao.propoe(lance)
 
         self.__carteira -= valor
+
+    def _valor_eh_valido(self, valor):
+        return valor <= self.__carteira
 
 
 class Lance:
@@ -31,21 +36,34 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
+        self.maior_lance = 0.0
+        self.menor_lance = 0.0
 
     @property
     def lances(self):
         return self.__lances[:] #Método GET devolve uma cópia rasa de self.__lances
 
     def propoe(self, lance: Lance):
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
+        if self._lance_eh_valido(lance):
+            if not self._tem_lances():
                 self.menor_lance = lance.valor
+            self.maior_lance - lance.valor
 
             self.__lances.append(lance)
-        else:
-            raise ValueError("Erro ao propor lance!!!")
+
+    def _tem_lances(self):
+        return self.__lances
+
+    def _usuarios_diferentes(self, lance):
+        if self.__lances[-1].usuario != lance.usuario:
+            return True
+        raise LanceInvalido("O mesmo usuário não pode dar dois lances seguidos!!!")
+
+    def _valor_maior_que_o_lance_anterior(self, lance):
+        if lance.valor > self.__lances[-1].valor:
+            return True
+        raise LanceInvalido("O valor do lance deve ser maior que o lance anterior!!!")
+
+    def _lance_eh_valido(self, lance):
+        return not self.__lances or self._usuarios_diferentes(lance) and self._valor_maior_que_o_lance_anterior(lance)
 
--- tests/test_leilao.py
+++ tests/test_leilao.py
@@ -1,26 +1,20 @@
 from unittest import TestCase
+
+from leilao.excecoes import LanceInvalido
 from leilao.leilao_project.dominio import Usuario, Lance, Leilao
 
 
 class TestLeilao(TestCase):
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             self.leilao.propoe(self.lance_david)
             self.leilao.propoe(self.lance_william)
 
 
-    def test_deve_retornar_o_mesmo_valor_para_o_maior_lance_quando_leilao_possuir_apenas_um_lance(self):
-        self.leilao.propoe(self.lance_david)
-
-        menor_valor_esperado = 500
-        maior_valor_esperado = 500
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_leilao_possuir_tres_lances_(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             pessoa3 = Usuario("David William", 500)
             lance_david_william = Lance(pessoa3, 400)
 
@@ -36,7 +30,7 @@ class TestLeilao(TestCase):
         self.assertEqual(1, quantidade_de_lances_recebido)
 
     def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             pessoa4 = Usuario("Nova Pessoa", 500)
             lance_nova_pessoa = Lance(pessoa4, 350)
 
@@ -51,12 +45,12 @@ class TestLeilao(TestCase):
     def test_nao_deve_permitir_propor_lance_caso_usuario_seja_o_mesmo(self):
         lance_david_novo = Lance(self.pessoa1, 390)
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             self.leilao.propoe(self.lance_david)
             self.leilao.propoe(lance_david_novo)
 
     def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             self.leilao.propoe(self.lance_david)
             self.leilao.propoe(self.lance_william)
 
